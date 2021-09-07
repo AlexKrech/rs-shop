dc694fe029f21a10a91089a8aaa7b577ae0afa65
Alex Krech <alexkrech1997@gmail.com>
2021-09-07 15:32:29 +0300

    feat: implement location service

---
+++ src/app/core/services/location.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
+import { URL_TO_CITIES } from 'src/app/shared/consts';
+
+import { ILocationByIp } from 'src/app/shared/models/location-by-ip';
+import {
+  ILocationResponseData,
+  ILocationsBySearchString,
+} from 'src/app/shared/models/location-by-search-string';
+import { IAppState } from '../store/state/app.state';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocationService {
+  constructor(private http: HttpClient, private store: Store<IAppState>) {}
+
+  getLocationByIp() {
+    return this.http
+      .get<ILocationByIp>('http://ip-api.com/json/')
+      .pipe(map((data) => data.city));
+  }
+
+  getLocationSearchString(searchString: string) {
+    return this.http.get<ILocationsBySearchString>(URL_TO_CITIES).pipe(
+      map((data) => {
+        let result: ILocationResponseData[] | null = data.cities.filter(
+          (city) => city.name.toLowerCase().includes(searchString.toLowerCase())
+        );
+
+        if (result.length === 0) {
+          result = null;
+        }
+
+        return result;
+      })
+    );
+  }
+}
