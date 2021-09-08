601961abf54c3a5e0343c9a36a1a458424f58866
Alex Krech <alexkrech1997@gmail.com>
2021-09-08 21:41:08 +0300

    feat: add router to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-
 import { environment } from 'src/environments/environment';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { appReducer } from './core/store/reducers/app.reducers';
 import { LocationEffects } from './core/store/effects/location.effects';
+import { ShopFeatureModule } from './shop-feature/shop-feature.module';
+import { MainComponent } from './shop-feature/pages/main/main.component';
+import { NotFoundComponent } from './core/pages/not-found/not-found.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'main', component: MainComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   declarations: [AppComponent],
@@ -24,12 +32,14 @@ import { LocationEffects } from './core/store/effects/location.effects';
     BrowserAnimationsModule,
     SharedModule,
     CoreModule,
+    ShopFeatureModule,
     HttpClientModule,
     StoreModule.forRoot(appReducer),
     EffectsModule.forRoot([LocationEffects]),
     !environment.production
       ? StoreDevtoolsModule.instrument({ logOnly: environment.production })
       : [],
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { InfoBlockComponent } from './components/header/info-block/info-block.component';
 import { ChooseLocationComponent } from './components/choose-location/choose-location.component';
 import { NavBlockComponent } from './components/header/nav-block/nav-block.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -14,8 +16,9 @@ import { NavBlockComponent } from './components/header/nav-block/nav-block.compo
     ChooseLocationComponent,
     ChooseLocationComponent,
     NavBlockComponent,
+    NotFoundComponent,
   ],
-  imports: [CommonModule, FormsModule],
-  exports: [HeaderComponent, ChooseLocationComponent],
+  imports: [CommonModule, FormsModule, RouterModule],
+  exports: [HeaderComponent, ChooseLocationComponent, NotFoundComponent],
 })
 export class CoreModule {}
